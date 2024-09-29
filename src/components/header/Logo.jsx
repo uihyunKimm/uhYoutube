12692fa0ff48d61ad3c6e94c5502747b279b3b3a
uihyunkimm <dmlxld@gmail.com>
2024-09-29 23:12:11 +0900

    add logo animation

--- src/components/header/Logo.jsx
+++ src/components/header/Logo.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Logo = () => {
     return (
-        <h1 className='border-b-[1px] #fff'>
-            <a href="/">
-                <em aria-hidden='true' className='w-10 h-10 block bg-red-600 mr-2'></em>
-                <span className='text-xl leading-none font-extrabold uppercase'>webs<br/>Youtube</span>
+        <h1 className='border-b-[1px] border-white/10 '>
+            <a href="/" className='flex p-2 m-2'>
+                <em className='w-10 h-10 block bg-red-600 mr-2 animate-spin-logo aria-hidden'></em>
+                <span className='text-xl font-poppins leading-none font-normal'>UIHKK <p className='text-xl font-poppins leading-none font-thin text-white/70'>Link storage</p></span>
             </a>
         </h1>
     )
--- src/components/section/Main.jsx
+++ src/components/section/Main.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Main = () => {
+const Main = (props) => {
     return (
-        <main id="main" role="main" className='max-w-[2000px] min-h-svh my-0 mx-auto pl-64 bg-slate-700'>
-            main
+        <main id="main" role="main" className='max-w-full min-h-svh mx-0 my-auto pl-64 bg-slate-700'>
+            {props.children}
         </main>
     );
 };
